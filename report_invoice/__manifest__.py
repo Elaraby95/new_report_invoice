8672fcbbdcfd5a626376c4a78537804ff1868656
Elaraby95 <132716221+Elaraby95@users.noreply.github.com>
2023-10-03 02:59:37 +0300

    Add files via upload

---
+++ report_invoice/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "REPORT INVOICE",
+    "summary": "Modify Invoice report",
+    "depends": ["account"],
+    "data": [
+        # "report/layout.xml",
+        "report/invoice_action.xml",
+        "report/invoice_template.xml",
+
+    ],
+    'version': '1.0',
+    'installable': True,
+    'license': 'LGPL-3',
+}
